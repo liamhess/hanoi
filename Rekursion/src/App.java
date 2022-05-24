001a40ce492d81791435c5de676d34e0543f0bb0
liamhess <49128538+liamhess@users.noreply.github.com>
2022-05-24 11:02:17 +0000

    ende

--- Rekursion/src/App.java
+++ Rekursion/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Rekursion r = new Rekursion();
-        System.out.println(r.potenzrek(2));
+        System.out.println(r.berechneggt(15, 30));
     }
 }
--- Rekursion/src/Rekursion.java
+++ Rekursion/src/Rekursion.java
@@ -22,4 +22,14 @@ public class Rekursion {
             return 1;
         }
     }
+
+    public int berechneggt(int a, int b) {
+        int ggT = 1;
+        for (int i = 1; i <= Math.min(a,b); i++) {
+            if ((a%i==0) && (b%i==0)) {
+                ggT = i;
+            }
+        }
+        return ggT;
+    }
 }
